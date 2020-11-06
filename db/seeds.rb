099e3e3f01736f9fb4429c87e6e762c6952fcd5c
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-06 17:26:38 +0100

    install pundit gem and add group, user, grocery list seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,33 +4,33 @@
 
 #Categories
 
-puts '--Cleaning categories'
-Category.destroy_all
-puts 'Categories cleaned'
+# puts '--Cleaning categories'
+# Category.destroy_all
+# puts 'Categories cleaned'
 
-puts '// Creating categories...'
-fridge = Category.create!(name: 'Fridge')
-pantry = Category.create!(name: 'Pantry')
-freezer = Category.create!(name: 'Freezer')
-hygiene = Category.create!(name: 'Hygiene')
-puts 'Categories created'
+# puts '// Creating categories...'
+# fridge = Category.create!(name: 'Fridge')
+# pantry = Category.create!(name: 'Pantry')
+# freezer = Category.create!(name: 'Freezer')
+# hygiene = Category.create!(name: 'Hygiene')
+# puts 'Categories created'
 
 
 # Staples
 
-puts '--Cleaning staples'
-Staple.destroy_all
-puts 'Staples cleaned'
+# puts '--Cleaning staples'
+# Staple.destroy_all
+# puts 'Staples cleaned'
 
-puts '// Creating staples...'
-mustard = Staple.create!(name: 'Mustard', category: fridge)
-tahini = Staple.create!(name: 'Tahini', category: fridge)
-chickpeas = Staple.create!(name: 'Chickpeas', category: pantry)
-quinoa = Staple.create!(name: 'Quinoa', category: pantry)
-frozen_berries = Staple.create!(name: 'Frozen Berries', category: freezer)
-toilet_paper = Staple.create!(name: 'Toilet Paper', category: hygiene)
-toothpaste = Staple.create!(name: 'Toothpaste', category: hygiene)
-puts 'Staples created'
+# puts '// Creating staples...'
+# mustard = Staple.create!(name: 'Mustard', category: fridge)
+# tahini = Staple.create!(name: 'Tahini', category: fridge)
+# chickpeas = Staple.create!(name: 'Chickpeas', category: pantry)
+# quinoa = Staple.create!(name: 'Quinoa', category: pantry)
+# frozen_berries = Staple.create!(name: 'Frozen Berries', category: freezer)
+# toilet_paper = Staple.create!(name: 'Toilet Paper', category: hygiene)
+# toothpaste = Staple.create!(name: 'Toothpaste', category: hygiene)
+# puts 'Staples created'
 
 
 # Quantities
@@ -44,3 +44,38 @@ puts 'Staples created'
 # almost_empty = Quantity.create!(value: 'Almost empty')
 # empty = Quantity.create!(value: 'Empty')
 # puts "Quantities created"
+
+
+Groups
+
+puts '--Cleaning Groups'
+Group.destroy_all
+puts 'Groups cleaned'
+
+puts '// Creating Groups...'
+dupond = Group.create!(name: 'Dupond')
+puts "Groups created"
+
+
+Users
+
+puts '--Cleaning Users'
+User.destroy_all
+puts 'Users cleaned'
+
+puts '// Creating Users...'
+emilie_dupond = User.create!(username: 'Emilie-dup', email: 'Emilie@gmail.com', password: 'Emilie123', group: dupond, group_admin: true )
+jean_dupond = User.create!(username: 'Jean-dup', email: 'jean@gmail.com', password: 'Jean123', group: dupond, group_admin: false )
+puts "Users created"
+
+
+Users
+
+puts '--Cleaning Grocery Lists'
+GroceryList.destroy_all
+puts 'Grocery Lists cleaned'
+
+puts '// Creating Grocery Lists...'
+dupond_list = GroceryList.create!(name: 'Dupond', group: dupond)
+puts "Grocery Lists created"
+
