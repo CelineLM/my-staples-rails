663bf458ef8b5bbeaf841851a98d5321f2f189de
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-10 17:22:06 +0100

    add my_staple new form and #create + pundit for create

--- app/controllers/my_staples_controller.rb
+++ app/controllers/my_staples_controller.rb
@@ -2,12 +2,28 @@ class MyStaplesController < ApplicationController
   def index
     @grocery_list = current_user.group.grocery_lists.first
     @my_staples = @grocery_list.my_staples
+    @my_staple = MyStaple.new
     authorize @my_staples
   end
 
   def create
+    @my_staple = MyStaple.new(my_staple_params)
+    @grocery_list = current_user.group.grocery_lists.first
+    @my_staple.grocery_list = @grocery_list
+    authorize @my_staple
+    if @my_staple.save
+      redirect_to grocery_list_my_staples_path(@grocery_list)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def my_staple_params
+    params.require(:my_staple).permit(:staple_id, :quantity_id, :grocery_list_id)
   end
 end
--- app/policies/my_staple_policy.rb
+++ app/policies/my_staple_policy.rb
@@ -8,4 +8,8 @@ class MyStaplePolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def create?
+    true
+  end
 end
