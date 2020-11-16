2fdb931c318858177bf03db1ab514519a190ec01
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-16 18:37:27 +0100

    add non_staples list + #create + #destroy + pundit

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -10,6 +10,10 @@ class GroceryListsController < ApplicationController
     @my_staples = @grocery_list.my_staples
     @ms_almost_empty = @my_staples.select { |m_s| m_s.quantity.value === 'Almost empty' }
     @ms_empty = @my_staples.select { |m_s| m_s.quantity.value === 'Empty' }
+
+    @non_staples = @grocery_list.non_staples
+
+    @non_staple = NonStaple.new
     authorize @grocery_list
   end
 end
--- app/controllers/non_staples_controller.rb
+++ app/controllers/non_staples_controller.rb
@@ -1,7 +1,30 @@
 class NonStaplesController < ApplicationController
+  before_action :set_non_staple, only: :destroy
+
   def create
+    @grocery_list = current_user.group.grocery_list
+    @non_staple = NonStaple.new(non_staple_params)
+    @non_staple.grocery_list = @grocery_list
+    authorize @non_staple
+    @non_staple.save
+    redirect_to grocery_list_path(@grocery_list)
   end
 
   def destroy
+    @grocery_list = current_user.group.grocery_list
+    @non_staple.destroy
+    redirect_to grocery_list_path(@grocery_list)
   end
+
+  private
+
+  def set_non_staple
+    @non_staple = NonStaple.find(params[:id])
+    authorize @non_staple
+  end
+
+  def non_staple_params
+    params.require(:non_staple).permit(:name, :grocery_list_id)
+  end
+
 end
