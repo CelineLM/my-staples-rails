5172eb3d3b03c32645fda2268abca9a363061be2
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-24 21:16:53 +0100

    Add color for new staple button and h1

--- app/controllers/staples_controller.rb
+++ app/controllers/staples_controller.rb
@@ -1,4 +1,6 @@
 class StaplesController < ApplicationController
+  before_action :set_grocery_list
+
   def new
     @staple = Staple.new
     authorize @staple
@@ -8,7 +10,6 @@ class StaplesController < ApplicationController
     @staple = Staple.new(staple_params)
     authorize @staple
     if @staple.save
-      @grocery_list = current_user.group.grocery_list
       redirect_to grocery_list_my_staples_path(@grocery_list)
     else
       render :new
@@ -16,6 +17,10 @@ class StaplesController < ApplicationController
   end
 
   private
+
+  def set_grocery_list
+    @grocery_list = current_user.group.grocery_list
+  end
 
   def staple_params
     params.require(:staple).permit(:name, :category_id)
