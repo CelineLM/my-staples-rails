0345c150f17ea108aa9c1d42dc7f4de6130d97d5
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-26 18:53:21 +0100

    Add config database cleaner

---
+++ spec/features/user_updates_my_staple_to_full_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+# feature "User updates my_staple to full automatically" do
+#   scenario "successfully" do
+#     sign_in_as ""
+#   end
+# end
---
+++ spec/support/features/sign_in_as.rb
@@ -0,0 +1,9 @@
+# module Features
+#   def sign_in_as(email, password)
+#     visit root_path
+#     click_on "Login"
+#     fill_in "Email", with: email
+#     fill_in "Password", with: password
+#     click_on "Log in"
+#   end
+# end
