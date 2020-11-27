797699b3533b5a549ab1139fd124531d1ba79bb6
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-27 10:06:00 +0100

    Set default value for group_admin and group_id when user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   belongs_to :group
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_initialize :set_defaults
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  private
+
+  def set_defaults
+    self.group_admin = false
+    self.group_id = nil
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
-describe User, "instance variables specification" do
+describe User, "instance variables default values" do  
+  context "When user sign up" do
 
-    it "should have group_admin set to false " do
-      user = User.create(email: "someone@example.com", username: "Someone", password: "Someone123")
+    it "does not have group_admin set to false " do
+      user =  User.create(email: "someone@example.com", username: "Someone", password: "Someone123")
       expect(user.group_admin).to be false
     end
 
-    it "should have group set to nil" do
-      user = User.create(email: "someone@example.com", username: "Someone", password: "Someone123")
-      expect(user.group).to be_nil
+    it "does not have group set to nil" do
+      user =  User.create(email: "someone@example.com", username: "Someone", password: "Someone123")
+      expect(user.group_id).to be_nil
     end
+  end
+end
+
+describe User, "validations" do
+  it "is invalid without an email" do
+    user =  User.create(username: "Someone", password: "Someone123")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a username" do
+    user =  User.create(email: "someone@example.com", password: "Someone123")
+    expect(user).not_to be_valid
+  end
 end
