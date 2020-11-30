e32a58232ab58b4fd261cf916eef3f89be77c55f
Celine Mercier <mercier_celine@hotmail.fr>
2020-11-30 12:13:57 +0100

    Change validations and seed accordingly with columns removal

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,4 @@
 class GroupsController < ApplicationController
-  def new
-  end
-
   def create
-  end
-
-  def edit
-  end
-
-  def update
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,4 @@
 class Group < ApplicationRecord
   has_many :users
   has_one :grocery_list, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
 end
