31ba788904cff40ec8546d6e9b662f7cf5cf0097
Celine Mercier <mercier_celine@hotmail.fr>
2020-12-03 11:18:31 +0100

    Add URL but not working

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,13 @@
 class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
+    @url = generate_url({id: @group.id})
     authorize @group
   end
 
   private
 
-  def generate_url_with_params(params = {})
+  def generate_url(params = {})
     uri = URI('http://localhost:3000/users/sign_up')
     uri.query = params.to_query
     uri.to_s
