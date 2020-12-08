351d0119b2d5fe188612dcc90fc48684c9415c17
Celine Mercier <mercier_celine@hotmail.fr>
2020-12-08 20:42:05 +0100

    Add color to h1 for sign up and log in

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :staples, only: [:new, :create]
 
   resources :grocery_lists, only: [:show] do
-      resources :non_staples, only: [:create]
-      resources :my_staples, only: [:index, :create]
+    resources :non_staples, only: [:create]
+    resources :my_staples, only: [:index, :create]
   end
   resources :groups, only: [:show]
   resources :my_staples, only: [:update,:destroy]
