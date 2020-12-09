a8d0cccc1a845662025a89a38aecc22621417f61
Celine Mercier <mercier_celine@hotmail.fr>
2020-12-09 13:48:59 +0100

    Init mailer to add members to groups

---
+++ app/mailers/add_a_member_mailer.rb
@@ -0,0 +1,2 @@
+class AddAMemberMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/add_a_member_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/add_a_member_mailer
+class AddAMemberMailerPreview < ActionMailer::Preview
+
+end
